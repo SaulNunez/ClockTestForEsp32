a575fbba893b706e2936397b768edfc53c0702c9
Saul Nuñez <saul.nunez99@gmail.com>
2021-11-25 16:38:38 -0700

    Implementation of getting time from server from https://microcontrollerslab.com/getting-current-date-time-esp32-arduino-ide-ntp-server/

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,40 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include "time.h"
+#include "credentials.h"
+
+const char* ntpServer = "pool.ntp.org";
+const long  gmtOffset_sec = -7 * 60 * 60;   //Replace with your GMT offset (seconds)
+const int   daylightOffset_sec = 3600; //Replace with your daylight offset (seconds)
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  Serial.printf("Connecting to %s ", ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+  }
+  Serial.println("CONNECTED to WIFI");
+
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
 }
 
+void printLocalTime()
+{
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("Failed to obtain time");
+    return;
+  }
+  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+}
+
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  while(true){
+    printLocalTime();
+    delay(5000);
+  }
 }
